dd2696ae229bda71107e9fc72bf36a243ebffafb
MarvinGM <marvinmahilum091197@gmail.com>
2021-06-02 15:54:40 +0800

    Initial Commit

---
+++ MyFirstSwiftUI/MyFirstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstSwiftUIApp.swift
+//  MyFirstSwiftUI
+//
+//  Created by Amben on 6/2/21.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
